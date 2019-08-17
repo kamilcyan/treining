50ebf8216a6e0606c768ec26cd3b01051c177d8c
kamilcyan <kamil.lucjanek@wp.pl>
2019-08-17 08:23:12 +0200

    pierwszy

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+
+public class Program
+{
+    public static void Main()
+    {
+        getContent();
+
+        async Task TestReader(System.IO.Stream stream)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.Async = true;
+
+            using (XmlReader reader = XmlReader.Create(@"D:\ProjektyCsh\Trening\ConsoleApp11\ConsoleApp11\file.xml", settings))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        if (reader.IsEmptyElement)
+                            Console.WriteLine("<{0}/>", reader.Name);
+                        else
+                        {
+                            Console.Write("<{0}> ", reader.Name);
+                            reader.Read(); // Read the start tag.
+                            if (reader.IsStartElement())  // Handle nested elements.
+                                Console.Write("\r\n<{0}>", reader.Name);
+                            Console.WriteLine(reader.ReadString());  //Read the text content of the element.
+                        }
+                    }
+                }
+            }
+        }
+
+        Console.ReadLine();
+    }
+
+    private static void getContent()
+    {
+
+        var url = "https://www.yr.no/place/Poland/Lublin/Pu%C5%82awy/forecast.xml";
+
+        var httpClient = new HttpClient();
+        var html = httpClient.GetStringAsync(url);
+        
+        System.IO.File.WriteAllText(@"D:\ProjektyCsh\Trening\ConsoleApp11\ConsoleApp11\file.xml", html.Result);
+    }
+}
