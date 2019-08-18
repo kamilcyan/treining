32b6dffb592422772193d966e1ec88cb1f7f6a2a
kamilcyan <kamil.lucjanek@wp.pl>
2019-08-18 06:41:23 +0200

    hmm

--- Program.cs
+++ Program.cs
@@ -7,34 +7,36 @@ public class Program
 {
     public static void Main()
     {
-        getContentToFile();
+        XmlReader reader = XmlReader.Create(@"d:\git\Treining\file.xml");
 
-        using (XmlReader reader = XmlReader.Create(@"d:\git\Treining\file.xml"))
-        {
-            while (reader.Read())
-            {
-                if (reader.IsStartElement())
-                {
-                    //return only when you have START tag  
-                    switch (reader.Name.ToString())
-                    {
-                        case "Name":
-                            Console.WriteLine("Name of the Element is : " + reader.ReadString());
-                            break;
-                        case "Location":
-                            Console.WriteLine("Your Location is : " + reader.ReadString());
-                            break;
-                    }
-                }
-                Console.WriteLine("");
-            }
-        }
+        GetContentToFile();
+        ReadFromXml(reader);
+
+
+        //using (XmlReader reader = XmlReader.Create(@"d:\git\Treining\file.xml"))
+        //{
+        //    while (reader.Read())
+        //    {
+        //        if (reader.IsStartElement())
+        //        {
+        //            //return only when you have START tag  
+        //            switch (reader.Name.ToString())
+        //            {
+        //                case "Name":
+        //                    Console.WriteLine("Name of the Element is : " + reader.ReadString());
+        //                    break;
+        //                case "Location":
+        //                    Console.WriteLine("Your Location is : " + reader.ReadString());
+        //                    break;
+        //            }
+        //        }
+        //        Console.WriteLine("");
+        //    }
+        //}
         Console.ReadKey();
-
-        Console.ReadLine();
     }
 
-    private static void getContentToFile()
+    private static void GetContentToFile()
     {
 
         var url = "https://www.yr.no/place/Poland/Lublin/Pu%C5%82awy/forecast.xml";
@@ -44,4 +46,44 @@ public class Program
         
         System.IO.File.WriteAllText(@"D:\git\Treining\file.xml", html.Result);
     }
+
+    public static void ReadFromXml(XmlReader reader)
+    {
+        reader.MoveToContent();
+
+        // Read node attributes
+        string name = reader.GetAttribute("name");
+        string sunRise = reader.GetAttribute("sun rise");
+
+        if (reader.IsEmptyElement) { reader.Read(); return; }
+
+        reader.Read();
+        while (!reader.EOF)
+        {
+            if (reader.IsStartElement())
+            {
+                switch (reader.Name)
+                {
+                    // Read element for a property of this class
+                    case "name":
+                        name = reader.ReadElementContentAsString();
+                        break;
+
+                    // Starting sub-list
+                    case "sun rise":
+                        sunRise = reader.ReadElementContentAsString();
+                        break;
+
+                    //default:
+                        //.Skip();
+                }
+            }
+            else
+            {
+                reader.Read();
+                break;
+            }
+        }
+    }
 }
+
