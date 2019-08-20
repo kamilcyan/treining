95b77013604a9b65ce0059fd0fcd74b604ff1add
kamilcyan <kamil.lucjanek@wp.pl>
2019-08-20 05:22:33 +0200

    selecting by date works

--- Program.cs
+++ Program.cs
@@ -7,10 +7,20 @@ public class Program
 {
     public static void Main()
     {
-       
-
+        bool isSameDate;
+        int liczba = 0;
         GetContentToFile();
         //ReadFromXml(reader);
+
+        string [] times = new string[40];
+        string[] symbols = new string[40];
+        string[] windDir = new string[40];
+        string[] temperatures = new string[40];
+
+        int licznikTemp = 0;
+        int licznikWindDir = 0;
+        int licznikTime = 0;
+        int licznikSymb = 0;
 
         using (XmlReader reader = XmlReader.Create(@"D:\git\Treining\file.xml"))
         {
@@ -44,52 +54,101 @@ public class Program
                         Console.WriteLine(time);
                         Console.WriteLine(reader.ReadString());
                         string attribute;
-                        attribute = reader["from"].Substring(0,10);
+                        attribute = reader["from"].Substring(0, 10);
                         date = attribute;
                         Console.WriteLine("time from : " + attribute);
-                        attribute = reader["to"].Substring(11,8);
+                        attribute = reader["to"]/*.Substring(11,8)*/;
                         Console.WriteLine("time to : " + attribute);
 
-                        setDate(date);
+
+                        //reader.MoveToAttribute("symbol");
+                        time = reader.Name.ToString();
+                        Console.WriteLine(time);
+                        attribute = reader["name"];
+                        Console.WriteLine("cloudines : " + attribute);
+                        attribute = reader["code"];
+                        Console.WriteLine("wind direction : " + attribute);
+                        reader.MoveToNextAttribute();
+                        time = reader.Name.ToString();
+                        Console.WriteLine(time);
+                        reader.MoveToNextAttribute();
+                        time = reader.Name.ToString();
+                        Console.WriteLine(time);
+                        reader.MoveToNextAttribute();
+                        time = reader.Name.ToString();
+                        Console.WriteLine(time);
+                        reader.MoveToNextAttribute();
+
+                        reader.MoveToAttribute("symbol");
+                        time = reader.Name.ToString();
+                        Console.WriteLine(time);
+                        reader.MoveToNextAttribute();
+                        time = reader.Name.ToString();
+                        Console.WriteLine(time);
+                        //setDate(date);
                     }
-                    reader.MoveToAttribute("symbol");
-                    Console.WriteLine("Your Type is : " + reader.Name.ToString());
-                    void setDate(string dat)
-                    {
-                        string dateToday = DateTime.Now.Year + "-" + "0" + DateTime.Now.Month + "-" + DateTime.Now.Day;
-                        Console.WriteLine(dateToday);
-                        Console.WriteLine(date);
-                        if (reader.Name.ToString() == "symbol")
-                        {
 
-
-                            if (dateToday == date)
-                            {
-                                Console.WriteLine("hola");
-                                string attribute;
-                                attribute = reader["name"];
-                                Console.WriteLine("clouds : " + attribute);
-                            }
-                        }
-                    }
-
-
-                    //Console.WriteLine("Your Location is : " + country);
-
-                    //return only when you have START tag  
-                    //switch (reader.Name.ToString())
+                    //void setDate(string dat)
                     //{
-                    //    case "sun rise":
-                    //        Console.WriteLine("Name of the Element is : " + reader.ReadString());
-                    //        break;
-                    //    case "country":
-                    //        Console.WriteLine("Your Location is : " + reader.ReadString());
-                    //        break;
-                    //}
+                    //    string dateToday = "2019-08-19";
+                    //        /*DateTime.Now.Year + "-" + "0" + DateTime.Now.Month + "-" + DateTime.Now.Day;*/
+                    //    Console.WriteLine(dateToday + date);
+
+                    //    if (date == dateToday)
+                    //    {
+                    //        Console.WriteLine("time fromljnkjhoho : " );
+                    //Console.WriteLine(liczba);
+
+                    if (reader.Name.ToString() == "time")
+                    {
+                        string attribute;
+                        attribute = reader["from"];
+                        attribute = reader["to"];
+                        times[licznikTime] = reader["from"].Substring(0, 10);
+                        licznikTime++;
+                    }
+
+
+                    if (reader.Name.ToString() == "symbol")
+                    {
+                        string attribute;
+                        attribute = reader["name"];
+                        symbols[licznikSymb] = reader["name"];
+                        licznikSymb++;
+                    }
+
+                    if (reader.Name.ToString() == "windDirection")
+                    {
+                        string attribute;
+                        attribute = reader["code"];
+                        windDir[licznikWindDir] = reader["code"];
+                        licznikWindDir++;
+                    }
+
+                    if (reader.Name.ToString() == "temperature")
+                    {
+                        string attribute;
+                        attribute = reader["value"];
+                        temperatures[licznikTemp] = reader["value"];
+                        licznikTemp++;
+                    }
+
+                    liczba++;
+
+
                 }
 
-
             }
+
+            Console.WriteLine("temp first day : " + temperatures[0]);
+            Console.WriteLine("cloudines first day : " + symbols[0]);
+            Console.WriteLine("wind first day : " + windDir[0]);
+            Console.WriteLine("time first day : " + times[0]);
+
+            Console.WriteLine("temp second day : " + temperatures[1]);
+            Console.WriteLine("cloudines second day : " + symbols[1]);
+            Console.WriteLine("wind second day : " + windDir[1]);
+            Console.WriteLine("time second day : " + times[1]);
         }
         //}
         Console.ReadKey();
